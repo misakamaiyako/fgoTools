f3f7de4cc5cb471c8b0136c099167ce982e8c2fb
misaka <misaka@github.com>
2018-10-31 18:07:08 +0800

    更新礼装信息，更改目录结构

---
+++ download/suitBat.js
@@ -0,0 +1,37 @@
+let http = require('https'),
+    fs = require('fs');
+let json  = require('../data/database/suit.json');
+const length = json.length;
+let downList = [];
+let errorList = [];
+for(let i =1;i<=length;i++){
+    downList.push(
+        new Promise(resolve => {
+            http.get(`https://fgowiki.com/guide/equipdetail/${i}?p=pc`, function(response) {
+                let chunk = '';
+                response.on('data', (d) => {
+                    chunk+=d;
+                });
+                response.on('end', () => {
+                    resolve();
+                    try{
+                        let detail = chunk.toString().match(/\[\{(.|\n)+\}\]/)[0];
+                        detail = JSON.parse(detail);
+                        json[i-1].INTRO = detail[0].INTRO;
+                        json[i-1].ILLUST = detail[0].ILLUST;
+                    } catch (e) {
+                        errorList.push({id:i})
+                    }
+                });
+            });
+        })
+    )
+}
+Promise.all(downList).then(()=>{
+    let writerStream = fs.createWriteStream(`../data/database/suit.json`,{flags :'r+'});
+    writerStream.write(JSON.stringify(json),'UTF8');
+    writerStream.end();
+    let errorWriterStream = fs.createWriteStream(`../data/database/UNcatchSuit.json`,{flags :'r+'});
+    errorWriterStream .write(JSON.stringify(errorList),'UTF8');
+    errorWriterStream .end();
+});
