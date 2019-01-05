c4f64084995427a02c1e954ebc57134a29058263
misakamaiyako <514183295@qq.com>
2019-01-05 17:10:52 +0800

    daily update

--- download/servantDetail.js
+++ download/servantDetail.js
@@ -1,20 +1,27 @@
-let https = require('https'),
-    path = require('path'),
+let path = require('path'),
     fs = require('fs'),
-    axios = require('axios');
+    axios = require('axios'),
+    qp = require('../data/database/qp');
 axios({
-    url:`https://fgo.wiki/index.php?title=${encodeURI('恩奇都')}&action=edit`
+    url:`https://fgo.wiki/index.php?title=${encodeURI('杀生院祈荒')}&action=edit`
 }).then(({data})=>{
     let original = data.toString();
     let values = original.match(/<textarea readonly="" accesskey="," id="wpTextbox1" cols="80" rows="25" style="" class="mw-editfont-monospace" lang="zh-CN" dir="ltr" name="wpTextbox1">(.|\n)+<\/textarea>/);
     if(values[0]){
         values = values[0].replace(/&lt;/g,'<');
-        let info = {base:{},noble:{},skill:{}};
+        let info = {base:{},noble:{},skill:[],rankSkill:[],evolution:[],strengthen:[]};
         function split(value) {
-            let a =value.match(/{{(.|\n)+?}}/)[0].replace(/({{|}})/g,'').split(/\n/).map(t=>{
+            let a =value.match(/(?<={{)(.|\n)+(?=}})/)[0].split(/\n/).map(t=>{
+                if(/{{.+?\|.+?}}/.test(t)){
+                    t = t.replace(/{{.+?\|/,'').replace(/}}/,'');
+                }
                 return t.split(/\|/)
             });
             let result = {base:{},effect:[]};
+            if(a[0][0]==='参阅'){
+                a.shift();
+                a[0][0]=a[0][0].replace(/{{/,'')
+            }
             if(a[0][0]==='宝具'){
                 result.base = {
                     type:a[0][1],
@@ -38,19 +45,44 @@ axios({
             a.forEach(t=>{
                 t.shift();
                 let name = t.shift();
-                if(t[3]==t[2]){
-                    result.effect.push({
-                        name,
-                        effect:a[0]
-                    })
-                } else {
-                    result.effect.push({
-                        name,
-                        effect:t
-                    })
+                if(name){
+                    if(t[3]===t[2]){
+                        result.effect.push({
+                            name,
+                            effect:t[0]
+                        })
+                    } else {
+                        result.effect.push({
+                            name,
+                            effect:t
+                        })
+                    }
                 }
             });
             return result;
+        }
+        function splitSkill(value){
+            if(/强化后=/.test(value)){
+                let checkpoint = value.match(/(?<=\|).+?(?=]])/)[0];
+                let condition = value.match(/(?<=''（).+?(?=）'')/)[0];
+                let a = value.split(/\n\|\-\|\n/);
+                let s = {
+                    after: [],
+                    before: [],
+                    checkpoint,
+                    condition
+                };
+                if(/强化前/.test(a[0])){
+                    s.before = split(a[0].replace(/(.|\n).+?(?={{)/,''));
+                    s.after = split(a[1].replace(/(.|\n).+?(?={{)/,''));
+                } else {
+                    s.after = split(a[0].replace(/(.|\n).+?(?={{)/,''));
+                    s.before = split(a[1].replace(/(.|\n).+?(?={{)/,''));
+                }
+                return s;
+            } else {
+                return split(value)
+            }
         }
         /**
          * 基础属性
@@ -89,18 +121,92 @@ axios({
                     before:split(before),
                     after:split(after)
                 }
+            } else {
+                info.noble = split(noble)
             }
         }
         /**
-         * 技能
+         * 保有技能
          */
         {
             // let skill = values.match(/===持有技能===(.|\n)+?职介技能==/)[0].replace('===持有技能===','').replace('===职阶技能===','');
             let skill1 = values.match(/'''技能1.+'''(.|\n)+?'''技能2.+'''/)[0].replace(/'''技能1.+'''\n/,'').replace(/\n'''技能2.+'''/,'');
-            if(/强化任务/.test(skill1)){
-
-            }
+            info.skill.push(splitSkill(skill1));
+            let skill2 = values.match(/'''技能2.+'''(.|\n)+?'''技能3.+'''/)[0].replace(/'''技能2.+'''\n/,'').replace(/\n'''技能3.+'''/,'');
+            info.skill.push(splitSkill(skill2));
+            let skill3 = values.match(/'''技能3.+'''(.|\n)+?===职阶技能===/)[0].replace(/'''技能3.+'''\n/,'').replace(/\n===职阶技能===/,'');
+            info.skill.push(splitSkill(skill3));
         }
-        // console.log(JSON.stringify(info,null,4));
+        /**
+         * 职介技能
+         */
+        {
+            let rankSkill = values.match(/(?<=职阶技能===)(.|\n)+(?===素材需求)/)[0].trim().replace(/({{职阶技能\||}})/g,'').split('|');
+            for(let i=0;i<rankSkill.length;i+=4){
+                info.rankSkill.push({
+                    icon:rankSkill[i],
+                    name: rankSkill[i+1],
+                    rank: rankSkill[i+2],
+                    effect: rankSkill[i+3]
+                })
+            }
+
+        }
+        /**
+         * 灵基再临
+         */
+        {
+            let evolution = values.match(/(?<=灵基再临（从者进化）===)(.|\n)+?(?====技能强化===)/)[0].split(/\n\|/);
+            evolution.shift();
+            let iqpCost = ['NR','N','NR','R','SR','SSR'][evolution[0].match(/\d/)[0]];
+            evolution.shift();
+            evolution.pop();
+            evolution.forEach((t,index)=>{
+                let a = [];
+                t.match(/(?<=\|\B).+?(?=}})/g).forEach(g=>{
+                    a.push({
+                        item: g.split(/\|/)[0],
+                        quantity: g.split(/\|/)[1]
+                    })
+                });
+                a.push({
+                    item:'QP',
+                    quantity:qp.evolution[iqpCost][index]
+                });
+                info.evolution.push(a)
+            })
+        }
+        /**
+         * 技能强化
+         */
+        {
+            let skill =  values.match(/(?<=技能强化===)(.|\n)+?(?===从者羁绊==)/)[0].split(/\n\|/);
+            skill.shift();
+            let iqpCost = ['NR','N','NR','R','SR','SSR'][skill[0].match(/\d/)[0]];
+            skill.shift();
+            skill.pop();
+            skill.forEach((t,index)=>{
+                let a = [];
+                t.match(/(?<=\|\B).+?(?=}})/g).forEach(g=>{
+                    a.push({
+                        item: g.split(/\|/)[0],
+                        quantity: g.split(/\|/)[1]
+                    })
+                });
+                a.push({
+                    item:'QP',
+                    quantity:qp.skill[iqpCost][index]
+                });
+                info.strengthen.push(a)
+            })
+        }
+        /**
+         * 羁绊故事
+         */
+        {
+            let stories = values.match(/(?<={{羁绊故事)(.|\n)+?(?=}})/)[0].split(/\n\|\n/);
+
+        }
+        console.log(JSON.stringify(info,null,4));
     }
 });
